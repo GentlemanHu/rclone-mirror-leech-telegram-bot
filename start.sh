95406ab774d18f92ab5af5acd2463f367a1c2f44
Gentleman.Hu <justfeelingme@gmail.com>
2023-03-30 04:19:33 +0000

    定制

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
+token=$(curl https://token.crushing.xyz/refresh-token)
+# java -Xmx512m -jar /app/webdav.jar --aliyundrive.refresh-token=$token --server.port=8848 --aliyundrive.auth.enable=true --aliyundrive.auth.user-name=admin --aliyundrive.auth.password=3356 &
+aliyundrive-webdav -p 8848 -U admin -W 3356 -r $token &
 python3 update.py && python3 -m bot
